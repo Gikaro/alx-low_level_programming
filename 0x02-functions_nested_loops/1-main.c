d35a3e78537462d91cc9273c05fc1eb82ee6a65d
root <gikarocaleb14@gmail.com>
2022-03-15 14:49:14 -0700

    alphabet in lower

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ * followed by a new line.
+ * Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately
+ */
+
+void print_alphabet(void)
+{
+return (0);
+}
