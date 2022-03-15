7f72c9ac04bac19418d3c3c5d5d8b6a81f55c6be
root <gikarocaleb14@gmail.com>
2022-03-15 15:19:17 -0700

    alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet, in lowercase,
- * followed by a new line.
- * Return: On success 1.
- *  On error, -1 is returned, and errno is set appropriately
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-void print_alphabet(void)
+int main(void)
 {
-return (0);
+	print_alphabet();
+	return (0);
 }
