dc73e1abbc3ab00b75e73e70e246fa20267eda03
root <gikarocaleb14@gmail.com>
2022-03-15 08:49:09 -0700

    pushing files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - main block
+ * Description: Printing putchat
+ * Return: Always 0 (Success)
+ */
+int _putchar(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
